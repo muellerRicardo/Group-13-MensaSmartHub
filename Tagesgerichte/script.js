363bc1aa8e91292d563ebd2c4f54d727a820afbd
DanielHStudium <da-hupe@t-online.de>
2024-01-20 12:42:27 +0100

    added missing meals and ingredients

--- Tagesgerichte/script.js
+++ Tagesgerichte/script.js
@@ -1,7 +1,30 @@
 var days = ["Montag, 01.01.2024", "Dienstag, 02.01.2024", "Mittwoch, 03.01.2024"];
+
 var meal_1 = ["Essen1_1.jpg", "Essen1_2.jpg", "Essen1_3.jpg"];
-var details_1 = [["209kcal/876kJ", "7,9g", "26,5g", "0,7g", "7,0g", "0,8g", "1,4g"], ["938kcal/3927kJ", "43,8g", "93,8g", "3,9g", "37,8g", "5,3g", "7,7g"], ["581kcal/2430,9kJ", "20g", "48g", "0g", "51g", "1g", "3,4g"]];
+var meal_2 = ["Essen2_1.jpg", "Essen2_2.jpg", "Essen2_3.jpg"];
+var meal_3 = ["Essen3_1.jpg", "Essen3_2.jpg", "Essen3_3.jpg"];
+var meal_4 = ["Essen4_1.jpg", "Essen4_2.jpg", "Essen4_3.jpg"];
+var meal_5 = ["Essen5_1.jpg", "Essen5_2.jpg", "Essen5_3.jpg"];
+var meal_6 = ["Essen6_1.jpg", "Essen6_2.jpg", "Essen6_3.jpg"];
+
+var details_1 = [["209kcal/876kJ", "7,9g", "26,5g", "0,7g", "7,0g", "0,8g", "1,4g"], ["938kcal/3927kJ", "43,8g", "93,8g", "3,9g", "37,8g", "5,3g", "7,7g"], ["581kcal/2430kJ", "20g", "48g", "0g", "51g", "1g", "3,4g"]];
+
+var details_2 = [["550kcal", "6g", "30g", "4g", "25g", "8g", "2g"], ["600kcal", "50g", "20g", "2g", "4g", "2g", "2,8g"], ["490kcal", "15g", "58g", "12g", "91g", "13g", "2g"]];
+
+var details_3 = [["1800g", "40g", "20g", "5,5g", "2g", "4g", "3g"], ["1200kcal", "30g", "28g", "13g", "3g", "3g", "4,2g"], ["800kcal", "17g", "46g", "19g", "44g", "33g", "12g"]];
+
+var details_4 = [["328kcal/920kJ", "18g", "13g", "0,8g", "1,1g", "2g", "1,3g"], ["835kcal", "20,3g", "15,4g", "5g", "8,9g", "3,3g", "10g"], ["440kcal", "29g", "4g", "1g", "29g", "4g", "1,1g"]];
+
+var details_5 = [["2000kcal", "50g", "30g", "18g", "11g", "12g", "5g"], ["2500kcal", "77g", "34g", "7,9g", "28,9g", "2,2g", "11g"], ["2300kcal", "88g", "13g", "12g", "39g", "2,2g", "1,9g"]];
+
+var details_6 = [["2100kcal", "70g", "40g", "15g", "5g", "25g", "4g"], ["900kcal", "23g", "55g", "8,8g", "15g", "3,3g", "18g"], ["1200kcal", "20g", "10g", "11g", "12g", "1,3g", "1,4g"]];
+
 var preis_1 = ["4,00€", "3,50€", "6,00€"];
+var preis_2 = ["1,99€", "1,60€", "1,30€"];
+var preis_3 = ["2,50€", "4,50€", "6,00€"];
+var preis_4 = ["4,00€", "3,50€", "6,00€"];
+var preis_5 = ["5,00€", "6,50€", "6,00€"];
+var preis_6 = ["5,00€", "5,50€", "3,50€"];
 let counter = 0;
 
 
@@ -11,6 +34,22 @@ function day_backwards(input) {
         document.getElementById("day").textContent = days[counter];
         document.getElementById("meal_1").src = meal_1[counter];
         document.getElementById("preis_1").textContent = preis_1[counter];
+
+        document.getElementById("meal_2").src = meal_2[counter];
+        document.getElementById("preis_2").textContent = preis_2[counter];
+
+        document.getElementById("meal_3").src = meal_3[counter];
+        document.getElementById("preis_3").textContent = preis_3[counter];
+
+        document.getElementById("meal_4").src = meal_4[counter];
+        document.getElementById("preis_4").textContent = preis_4[counter];
+
+        document.getElementById("meal_5").src = meal_5[counter];
+        document.getElementById("preis_5").textContent = preis_5[counter];
+
+        document.getElementById("meal_6").src = meal_6[counter];
+        document.getElementById("preis_6").textContent = preis_6[counter];
+
         updateDetails(input);
     }
 }
@@ -21,6 +60,23 @@ function day_forwards(input) {
         document.getElementById("day").textContent = days[counter];
         document.getElementById("meal_1").src = meal_1[counter];
         document.getElementById("preis_1").textContent = preis_1[counter];
+
+        document.getElementById("meal_2").src = meal_2[counter];
+        document.getElementById("preis_2").textContent = preis_2[counter];
+
+        document.getElementById("meal_3").src = meal_3[counter];
+        document.getElementById("preis_3").textContent = preis_3[counter];
+
+        document.getElementById("meal_4").src = meal_4[counter];
+        document.getElementById("preis_4").textContent = preis_4[counter];
+
+        document.getElementById("meal_5").src = meal_5[counter];
+        document.getElementById("preis_5").textContent = preis_5[counter];
+
+        document.getElementById("meal_6").src = meal_6[counter];
+        document.getElementById("preis_6").textContent = preis_6[counter];
+
+
         updateDetails(input);
     }
 }
@@ -33,6 +89,47 @@ function updateDetails(input) {
     document.getElementById("eiweiß_1").textContent = details_1[counter][4];
     document.getElementById("salz_1").textContent = details_1[counter][5];
     document.getElementById("ballaststoffe_1").textContent = details_1[counter][6];
+
+
+    document.getElementById("brennwert_2").textContent = details_2[counter][0];
+    document.getElementById("fett_2").textContent = details_2[counter][1];
+    document.getElementById("kohlenhydrate_2").textContent = details_2[counter][2];
+    document.getElementById("zucker_2").textContent = details_2[counter][3];
+    document.getElementById("eiweiß_2").textContent = details_2[counter][4];
+    document.getElementById("salz_2").textContent = details_2[counter][5];
+    document.getElementById("ballaststoffe_2").textContent = details_2[counter][6];
+
+    document.getElementById("brennwert_3").textContent = details_3[counter][0];
+    document.getElementById("fett_3").textContent = details_3[counter][1];
+    document.getElementById("kohlenhydrate_3").textContent = details_3[counter][2];
+    document.getElementById("zucker_3").textContent = details_3[counter][3];
+    document.getElementById("eiweiß_3").textContent = details_3[counter][4];
+    document.getElementById("salz_3").textContent = details_3[counter][5];
+    document.getElementById("ballaststoffe_3").textContent = details_3[counter][6];
+
+    document.getElementById("brennwert_4").textContent = details_4[counter][0];
+    document.getElementById("fett_4").textContent = details_4[counter][1];
+    document.getElementById("kohlenhydrate_4").textContent = details_4[counter][2];
+    document.getElementById("zucker_4").textContent = details_4[counter][3];
+    document.getElementById("eiweiß_4").textContent = details_4[counter][4];
+    document.getElementById("salz_4").textContent = details_4[counter][5];
+    document.getElementById("ballaststoffe_4").textContent = details_4[counter][6];
+
+    document.getElementById("brennwert_5").textContent = details_5[counter][0];
+    document.getElementById("fett_5").textContent = details_5[counter][1];
+    document.getElementById("kohlenhydrate_5").textContent = details_5[counter][2];
+    document.getElementById("zucker_5").textContent = details_5[counter][3];
+    document.getElementById("eiweiß_5").textContent = details_5[counter][4];
+    document.getElementById("salz_5").textContent = details_5[counter][5];
+    document.getElementById("ballaststoffe_5").textContent = details_5[counter][6];
+
+    document.getElementById("brennwert_6").textContent = details_6[counter][0];
+    document.getElementById("fett_6").textContent = details_6[counter][1];
+    document.getElementById("kohlenhydrate_6").textContent = details_6[counter][2];
+    document.getElementById("zucker_6").textContent = details_6[counter][3];
+    document.getElementById("eiweiß_6").textContent = details_6[counter][4];
+    document.getElementById("salz_6").textContent = details_6[counter][5];
+    document.getElementById("ballaststoffe_6").textContent = details_6[counter][6];
 }
 
 function toggleDropdown() {
